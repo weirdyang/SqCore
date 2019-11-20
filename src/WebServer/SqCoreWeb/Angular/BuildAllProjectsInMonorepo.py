0b3c6a35f1e5d449662addf33c13f7869ca902e7
Dr. George Antal <gyantal1@gmail.com>
2019-11-20 13:40:41 +0000

    using BuildAllProjectsInMonorepo.py in build. Changing it a little bit.

--- src/WebServer/SqCoreWeb/Angular/BuildAllProjectsInMonorepo.py
+++ src/WebServer/SqCoreWeb/Angular/BuildAllProjectsInMonorepo.py
@@ -6,30 +6,25 @@ import subprocess
 os.system("npm run build -- MarketDashboard --prod")
 os.system("npm run build -- HealthMonitor --prod")
 
+print ("!!!!!!!!!!!! Brotli-ing text files... (brotli(x64).exe runs only in Python x64) !!!!!!!!!!")
 for dir in os.walk("dist"):
-	print("directory"+dir[0])
+	print("directory: "+dir[0])
 	for file in dir[2]:
 		fileSplit = os.path.splitext(file)
-		print (fileSplit)
 		brotliNeeded = 0
 		if (fileSplit[1] == ".js"):
-			print ("    found js") 
 			brotliNeeded = 1
 		if (fileSplit[1] == ".json"):
-			print ("    found json") 
 			brotliNeeded = 1
 		if (fileSplit[1] == ".xml"):
-			print ("    found xml") 
 			brotliNeeded = 1
 		if (fileSplit[1] == ".css"):
-			print ("    found css") 
 			brotliNeeded = 1
 		if (fileSplit[1] == ".html"):
-			print ("    found html") 
 			brotliNeeded = 1
 		if (fileSplit[1] == ".txt"):
-			print ("    found txt") 
 			brotliNeeded = 1
 		if (brotliNeeded == 1):
-#			print ("!!!!!!!!!!!!      brotli  !!!!!!!!!!")
+			print (fileSplit)
+#			print ("!!!!!!!!!!!!      brotli(x64).exe (it runs only in Python x64) !!!!!!!!!!")
 			os.system(r"c:/windows/system32/brotli.exe " +dir[0]+ '/' + file + " --best --force --verbose")
