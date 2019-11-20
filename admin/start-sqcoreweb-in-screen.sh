4a52f48519ba0efa0e0af8d7acd4791c67747132
Dr. George Antal <gyantal1@gmail.com>
2019-11-20 23:23:32 +0000

    Webserver should auto start after server reboot. Being headless. Linux 'screen' functionality

---
+++ admin/start-sqcoreweb-in-screen.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Create a screen with a name and in detached mode
+screen -S "SqCoreWeb" -d -m
+# Send the command to be executed on your screen
+# The $ before the command is to make the shell parse the \n inside the quotes, and the newline is required to execute the command (like when you press enter).
+screen -r "SqCoreWeb" -X stuff $'cd /home/sq-vnc-client/SQ/WebServer/SqCoreWeb/src/WebServer/SqCoreWeb/bin/Release/netcoreapp3.0/publish\ndotnet SqCoreWeb.dll\n'
+#
+#this is how to kill a session
+#screen -X -S "SqCoreWeb" quit
+#
+# Then run 'crontab -e' and insert this:
+## run SqCore web server 20 sec after reboot
+#@reboot sleep 20 && /home/sq-vnc-client/SQ/admin/start-sqcoreweb-in-screen.sh
+
