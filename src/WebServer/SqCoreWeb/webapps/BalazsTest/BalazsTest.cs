5c0ad6d08ae72cfd1d741f737bf6b9d48bf686c6
Balazs Lukucz <lbalazs7@gmail.com>
2019-12-04 11:47:44 +0100

    New BalazsTest

---
+++ src/WebServer/SqCoreWeb/webapps/BalazsTest/BalazsTest.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SqCoreWeb.Controllers
+{
+    // public class ExampleMessage
+    // {
+    //     public string MsgType { get; set; } = String.Empty;
+
+    //     public string StringData { get; set; } = String.Empty;
+    //     public DateTime DateOrTime { get; set; }
+
+    //     public int IntData { get; set; }
+
+    //     public int IntDataFunction => 32 + (int)(IntData / 0.5556);
+
+    // }
+
+    [ApiController]
+    [Route("[controller]")]
+    [ResponseCache(CacheProfileName = "DefaultMidDuration")]
+    public class BalazsTestController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public BalazsTestController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ExampleMessage> Get()
+        {
+            Thread.Sleep(5000);     // intentional delay to simulate a longer process to crunch data. This can be removed.
+
+            var userEmailClaim = HttpContext?.User?.Claims?.FirstOrDefault(p => p.Type == @"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
+            string email = userEmailClaim?.Value  ?? "Unknown email";
+
+            var firstMsgToSend = new ExampleMessage
+            {
+                MsgType = "AdminMsg",
+                StringData = $"Cookies says your email is '{email}'.",
+                DateOrTime = DateTime.Now,
+                IntData = 0,                
+            };
+
+            string[] RandomStringDataToSend = new[]  { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+            var rng = new Random();
+            return (new ExampleMessage[] { firstMsgToSend }.Concat(Enumerable.Range(1, 5).Select(index => new ExampleMessage
+            {
+                MsgType = "Msg-type",
+                StringData = RandomStringDataToSend[rng.Next(RandomStringDataToSend.Length)],
+                DateOrTime = DateTime.Now.AddDays(index),
+                IntData = rng.Next(-20, 55)                
+            }))).ToArray();
+        }
+    }
+}
---
+++ src/WebServer/SqCoreWeb/wwwroot/webapps/BalazsTest/BalazsTest.ts
@@ -0,0 +1,44 @@
+export {}; // TS convention: To avoid top level duplicate variables, functions. This file should be treated as a module (and have its own scope) or not as a script (and share the global scope with other scripts (files)).
+
+// 1. Declare some global variables and hook on DOMContentLoaded() and window.onload()
+console.log('SqCore: Script BEGIN4');
+
+async function AsyncStartDownloadAndExecuteCbLater(url: string, callback: (json: any) => any) {
+    fetch(url)
+    .then(response => { // asynch long running task finishes. Resolves to get the Response object (http header, info), but not the full body (that might be streaming and arriving later)
+        console.log('SqCore.AsyncStartDownloadAndExecuteCbLater(): Response object arrived:');
+        if (!response.ok) {
+            return Promise.reject(new Error('Invalid response status'));
+        }
+        response.json().then(json => {  // asynch long running task finishes. Resolves to the body, converted to json() object or text()
+            // const jsonToStr = JSON.stringify(json).substr(0, 60) + '...';
+            // console.log('SqCore.AsyncStartDownloadAndExecuteCbLater():: data body arrived:' + jsonToStr);
+            callback(json);
+        });
+    })
+    .catch((err) => {
+        console.log('SqCore: Download error.');
+    });
+}
+
+function getDocElementById(id: string): HTMLElement {
+    return (document.getElementById(id) as HTMLElement);   // type casting assures it is not null for the TS compiler. (it can be null during runtime)
+}
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOMContentLoaded(). All JS were downloaded. DOM fully loaded and parsed.');
+});
+
+window.onload = function onLoadWindow() {
+    console.log('SqCore: window.onload() BEGIN. All CSS, and images were downloaded.'); // images are loaded at this time, so their sizes are known
+
+    AsyncStartDownloadAndExecuteCbLater('/BalazsTest', (json: any) => {
+        // const jsonToStr = JSON.stringify(json).substr(0, 60) + '...';
+        getDocElementById('DebugDataArrivesHere').innerText = '***"' + json[0].stringData + '"***';
+    });
+
+    console.log('SqCore: window.onload() END.');
+};
+
+console.log('SqCore: Script END');
