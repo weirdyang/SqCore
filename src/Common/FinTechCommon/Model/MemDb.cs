4de6cd9f94c4d662eb2fc01ffc4657d1df2f46fc
Dr. George Antal <gyantal1@gmail.com>
2020-03-31 18:22:38 +0100

    Alphabetical order in MemDb

--- src/Common/FinTechCommon/Model/MemDb.cs
+++ src/Common/FinTechCommon/Model/MemDb.cs
@@ -27,16 +27,18 @@ namespace FinTechCommon
 
         public static MemDb gMemDb = new MemDb();
         // alphabetical order for faster search. 
-        // SumMem: 2+10+10+4*5 = 42 years. 42*260*(2+4)= 66KB.
-        public List<Security> Securities { get; } = new List<Security>() { // to minimize mem footprint, only load the necessary dates (not all history). 1Year = 260*(2+4)=1560B = 1.5KB,  5y data is: 5*260*(2+4) = 7.8K
+        // RAM requirement: 1Year = 260*(2+4) = 1560B = 1.5KB,  5y data is: 5*260*(2+4) = 7.8K
+        // Max RAM requirement if need only AdjClose: 20years for 5K stocks: 5000*20*260*(2+4) = 160MB (only one data per day: DivSplitAdjClose.)
+        // Max RAM requirement if need O/H/L/C/AdjClose/Volume: 6x of previous = 960MB = 1GB
+        // current SumMem: 2+10+10+4*5 = 42 years. 42*260*(2+4)= 66KB.
+        public List<Security> Securities { get; } = new List<Security>() { // to minimize mem footprint, only load the necessary dates (not all history).
             new Security() { SecID = 1, Ticker = "GLD", ExpectedHistorySpan="5y"},                  // history starts on 2004-11-18
             new Security() { SecID = 2, Ticker = "QQQ", ExpectedHistorySpan="Date: 2010-01-01"},    // history starts on 1999-03-10. Full history would be: 32KB. 
             new Security() { SecID = 3, Ticker = "SPY", ExpectedHistorySpan="Date: 2010-01-01"},    // history starts on 1993-01-29. Full history would be: 44KB, 
             new Security() { SecID = 4, Ticker = "TLT", ExpectedHistorySpan="5y"},                  // history starts on 2002-07-30
-            new Security() { SecID = 5, Ticker = "VXX", ExpectedHistorySpan="Date: 2018-01-25"},    // history starts on 2018-01-25 on YF, because VXX was restarted. The previously existed VXX.B shares are not on YF.
             new Security() { SecID = 6, Ticker = "UNG", ExpectedHistorySpan="5y"},                  // history starts on 2007-04-18
-            new Security() { SecID = 7, Ticker = "USO", ExpectedHistorySpan="5y"}};                 // history starts on 2006-04-10
-
+            new Security() { SecID = 7, Ticker = "USO", ExpectedHistorySpan="5y"},                  // history starts on 2006-04-10
+             new Security() { SecID = 5, Ticker = "VXX", ExpectedHistorySpan="Date: 2018-01-25"}};  // history starts on 2018-01-25 on YF, because VXX was restarted. The previously existed VXX.B shares are not on YF.
 
         Timer m_reloadHistoricalDataTimer;
         DateTime m_lastReloadHistoricalData = DateTime.MinValue; // UTC
